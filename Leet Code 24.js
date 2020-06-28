4cfa9422cd9d3c780b68d533ff1f351a529f1482
umitInanc <67541790+umitInanc@users.noreply.github.com>
2020-06-28 13:47:35 +0300

    Add files via upload

---
+++ Leet Code 24.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    const prehead = { next: head }
+  
+    for (let p = prehead; p.next !== null && p.next.next !== null;) {
+      const p1 = p.next
+      const p2 = p1.next
+      p1.next = p2.next
+      p2.next = p1
+      p.next = p2
+      p = p1
+    }
+  
+    return prehead.next
+  };
